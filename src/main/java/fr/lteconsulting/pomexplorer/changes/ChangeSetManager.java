0f1e365082b6e8f8eabba1817902d7c8388097c1
Arnaud Tournier <tourniera.a-ssii@mipih.fr>
2015-06-09 14:42:03 +0200

    prepare change management

---
+++ src/main/java/fr/lteconsulting/pomexplorer/changes/ChangeSetManager.java
@@ -0,0 +1,137 @@
+package fr.lteconsulting.pomexplorer.changes;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+import fr.lteconsulting.pomexplorer.depanalyze.Location;
+
+/**
+ * Manages a set of changes.<br/>
+ * 
+ * As changes are added to this ChangeSet, they are processed to check whether other changes should be generated, or if
+ * some should be removed
+ * 
+ * - allows to transform changes according to the situation
+ * 
+ * - allows to trace back generated changes to their cause
+ * 
+ * - allows to check consistency between changes
+ */
+public class ChangeSetManager implements IChangeSet
+{
+	private final List<IChangeProcessor> processors = new ArrayList<>();
+	private final Set<ChangeInfo> changes = new HashSet<>();
+
+	public void addProcessor(IChangeProcessor processor)
+	{
+		processors.add(processor);
+	}
+
+	@Override
+	public void addChange(Change<? extends Location> change)
+	{
+		ChangeInfo info = new ChangeInfo(change);
+
+		changes.add(info);
+	}
+
+	@Override
+	public void removeChange(Change<? extends Location> change)
+	{
+		ChangeInfo info = new ChangeInfo(change);
+
+		changes.remove(info);
+	}
+
+	/**
+	 * Process change resolution
+	 */
+	public void resolveChanges()
+	{
+		while (true)
+		{
+			// while there are not yet processed changes
+			List<ChangeInfo> notProcessed = new ArrayList<>();
+			for (ChangeInfo info : changes)
+				if (!info.isProcessed())
+					notProcessed.add(info);
+
+			if (notProcessed.isEmpty())
+				break;
+
+			// process them : run each processor on it
+			for (ChangeInfo info : notProcessed)
+			{
+				processChange(info);
+
+				// mark them as processed
+				info.setProcessed(true);
+			}
+
+		}
+	}
+
+	private void processChange(ChangeInfo info)
+	{
+		for (IChangeProcessor processor : processors)
+			processor.processChange(info.getChange(), this);
+	}
+
+	private static class ChangeInfo
+	{
+		private final Change<? extends Location> change;
+
+		private boolean isProcessed;
+
+		public ChangeInfo(Change<? extends Location> change)
+		{
+			this.change = change;
+		}
+
+		public Change<? extends Location> getChange()
+		{
+			return change;
+		}
+
+		public void setProcessed(boolean isProcessed)
+		{
+			this.isProcessed = isProcessed;
+		}
+
+		public boolean isProcessed()
+		{
+			return isProcessed;
+		}
+
+		@Override
+		public int hashCode()
+		{
+			final int prime = 31;
+			int result = 1;
+			result = prime * result + ((change == null) ? 0 : change.hashCode());
+			return result;
+		}
+
+		@Override
+		public boolean equals(Object obj)
+		{
+			if (this == obj)
+				return true;
+			if (obj == null)
+				return false;
+			if (getClass() != obj.getClass())
+				return false;
+			ChangeInfo other = (ChangeInfo)obj;
+			if (change == null)
+			{
+				if (other.change != null)
+					return false;
+			}
+			else if (!change.equals(other.change))
+				return false;
+			return true;
+		}
+	}
+}
