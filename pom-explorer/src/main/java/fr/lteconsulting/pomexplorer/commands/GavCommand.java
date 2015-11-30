9946a32137b3d800f8c133f345b5987718b8f2fd
Arnaud Tournier <ltearno@gmail.com>
2015-11-30 07:25:21 +0100

    gav list performance

--- pom-explorer/src/main/java/fr/lteconsulting/pomexplorer/commands/GavCommand.java
+++ pom-explorer/src/main/java/fr/lteconsulting/pomexplorer/commands/GavCommand.java
@@ -9,70 +9,74 @@ import fr.lteconsulting.pomexplorer.WorkingSession;
 
 public class GavCommand
 {
-	@Help("list the session's GAVs")
-	public void main(WorkingSession session, ILogger log)
+	@Help( "list the session's GAVs" )
+	public void main( WorkingSession session, ILogger log )
 	{
-		list(session, log);
+		list( session, log );
 	}
 
-	@Help("list the session's GAVs")
-	public void list(WorkingSession session, ILogger log)
+	@Help( "list the session's GAVs" )
+	public void list( WorkingSession session, ILogger log )
 	{
-		list(session, null, log);
+		list( session, null, log );
 	}
 
-	@Help("list the session's GAVs, with filtering")
-	public void list(WorkingSession session, FilteredGAVs gavFilter, ILogger log)
+	@Help( "list the session's GAVs, with filtering" )
+	public void list( WorkingSession session, FilteredGAVs gavFilter, ILogger log )
 	{
-		log.html("<br/>GAV list filtered with '" + (gavFilter != null ? gavFilter.getFilter() : "no filter") + "' :<br/>");
-		if (gavFilter != null)
-			gavFilter.getGavs(session).stream().sorted((g1, g2) -> g1.toString().compareTo(g2.toString()))
-					.forEach(gav -> log.html(gav + "<br/>"));
+		StringBuilder sb = new StringBuilder();
+
+		sb.append( "<br/>GAV list filtered with '" + (gavFilter != null ? gavFilter.getFilter() : "no filter") + "' :<br/>" );
+		if( gavFilter != null )
+			gavFilter.getGavs( session ).stream().sorted( ( g1, g2 ) -> g1.toString().compareTo( g2.toString() ) )
+					.forEach( gav -> sb.append( gav + "<br/>" ) );
 		else
-			session.graph().gavs().stream().sorted((g1, g2) -> g1.toString().compareTo(g2.toString()))
-					.forEach(gav -> log.html(gav + "<br/>"));
+			session.graph().gavs().stream().sorted( ( g1, g2 ) -> g1.toString().compareTo( g2.toString() ) )
+					.forEach( gav -> sb.append( gav + "<br/>" ) );
+
+		log.html( sb.toString() );
 	}
 
-	@Help("analyze all the gav's dependencies and add them in the pom graph.")
-	public void add(WorkingSession session, ILogger log, Client client, GAV gav)
+	@Help( "analyze all the gav's dependencies and add them in the pom graph." )
+	public void add( WorkingSession session, ILogger log, Client client, GAV gav )
 	{
 		PomAnalyzer analyzer = new PomAnalyzer();
 
-		analyzer.fetchGavWithMaven(session, log, gav);
+		analyzer.fetchGavWithMaven( session, log, gav );
 
-		log.html("finished !<br/>");
+		log.html( "finished !<br/>" );
 	}
 
-	@Help("analyze gavs which have no associated project")
-	public void resolve(WorkingSession session, ILogger log, Client client)
+	@Help( "analyze gavs which have no associated project" )
+	public void resolve( WorkingSession session, ILogger log, Client client )
 	{
 		PomAnalyzer analyzer = new PomAnalyzer();
 
-		session.graph().gavs().stream().filter(gav -> session.projects().forGav(gav) == null)
-				.parallel().forEach(gav -> {
-					log.html("analyzing " + gav + "...<br/>");
-					analyzer.fetchGavWithMaven(session, log, gav);
-				});
+		session.graph().gavs().stream().filter( gav -> session.projects().forGav( gav ) == null )
+				.parallel().forEach( gav -> {
+					log.html( "analyzing " + gav + "...<br/>" );
+					analyzer.fetchGavWithMaven( session, log, gav );
+				} );
 
-		log.html("finished !<br/>");
+		log.html( "finished !<br/>" );
 	}
 
-	@Help("load a gav from repository and analyze it")
-	public void load(WorkingSession session, ILogger log, Client client, String gavString)
+	@Help( "load a gav from repository and analyze it" )
+	public void load( WorkingSession session, ILogger log, Client client, String gavString )
 	{
 		PomAnalyzer analyzer = new PomAnalyzer();
 
-		GAV gav = Tools.string2Gav(gavString);
-		if (gav == null)
+		GAV gav = Tools.string2Gav( gavString );
+		if( gav == null )
 		{
-			log.html(Tools.warningMessage("the string '" + gavString
-					+ "' is not parsable into a fully qualified GAV (groupId:artifactId:version) !"));
+			log.html( Tools.warningMessage( "the string '" + gavString
+					+ "' is not parsable into a fully qualified GAV (groupId:artifactId:version) !" ) );
 			return;
 		}
 
-		log.html("analyzing " + gav + "...<br/>");
-		analyzer.fetchGavWithMaven(session, log, gav);
+		log.html( "analyzing " + gav + "...<br/>" );
+		analyzer.fetchGavWithMaven( session, log, gav );
 
-		log.html("finished !<br/>");
+		log.html( "finished !<br/>" );
 	}
 }
