72b69394a5322a6345c8e2b28b7a643906f9ba87
Robert Stoll <rstoll@tutteli.ch>
2018-01-25 16:19:01 +0100

    define appropriate asserts in each test (#45)
    and other cleanups, reuse code instead of duplicating it etc.

--- pom-explorer-core/src/test/java/fr/lteconsulting/pomexplorer/AnalyzerTest.java
+++ pom-explorer-core/src/test/java/fr/lteconsulting/pomexplorer/AnalyzerTest.java
@@ -7,7 +7,11 @@ import static org.junit.Assert.fail;
 
 import java.io.File;
 import java.util.*;
+import java.util.stream.Collectors;
 
+import fr.lteconsulting.pomexplorer.graph.ProjectRepository;
+import fr.lteconsulting.pomexplorer.graph.relation.BuildDependencyRelation;
+import fr.lteconsulting.pomexplorer.graph.relation.DependencyRelation;
 import org.junit.Test;
 
 import fr.lteconsulting.pomexplorer.graph.PomGraph.PomGraphReadTransaction;
@@ -17,202 +21,246 @@ import fr.lteconsulting.pomexplorer.model.transitivity.Repository;
 
 public class AnalyzerTest
 {
+
+	private static final String PROJECT_A = "fr.lteconsulting:a:1.0-SNAPSHOT";
+	private static final String PROJECT_B = "fr.lteconsulting:b:1.0-SNAPSHOT";
+	private static final String PROJECT_C = "fr.lteconsulting:c:1.0-SNAPSHOT";
+	private static final String PROJECT_D = "fr.lteconsulting:d:1.0-SNAPSHOT";
+	private static final String PROJECT_E = "fr.lteconsulting:e:1.0-SNAPSHOT";
+
 	@Test
 	public void test01()
 	{
+		//arrange
 		Session session = new Session();
-
-		PomAnalysis.runFullRecursiveAnalysis( "testSets/set01", session, null, null, true, System.out::println );
-
-		assertEquals( 1, session.projects().size() );
-
-		System.out.println( "DEPENDENCIES" );
-		session.graph().read().dependencies( Gav.parse( "fr.lteconsulting:pom-explorer:1.1-SNAPSHOT" ) ).forEach(System.out::println);
-
-		System.out.println( "BUILD DEPENDENCIES" );
-		session.graph().read().buildDependencies( Gav.parse( "fr.lteconsulting:pom-explorer:1.1-SNAPSHOT" ) ).forEach(System.out::println);
-
-		System.out.println( "NULL VERSION GAVS" );
-		session.graph().read().gavs().stream()
-				.sorted(Comparator.comparing(Gav::toString))
-				.filter( gav -> gav.getVersion() == null )
-				.forEach(System.out::println);
-	}
-
-	@Test
-	public void test04()
-	{
-		Session session = new Session();
-
-		PomAnalysis.runFullRecursiveAnalysis( "testSets/set02", session, null, null, true, System.out::println );
-
-		assertEquals( 5, session.projects().size() );
-
-		session.projects().values().forEach( project ->
-		{
-			System.out.println( "PROJECT " + project );
-		} );
-	}
-
-	@Test
-	public void test05()
-	{
-		Session session = new Session();
-
-		PomAnalysis.runFullRecursiveAnalysis( "testSets/set05", session, null, null, true, System.out::println );
-
-		assertEquals( 2, session.projects().size() );
-
-		List<String> shouldBeMissing = new ArrayList<>();
-		shouldBeMissing.add( "fr.lteconsulting:d:1.0-SNAPSHOT" );
-		shouldBeMissing.add( "fr.lteconsulting:c:1.0-SNAPSHOT" );
-		shouldBeMissing.add( "fr.lteconsulting:c:1.0-SNAPSHOT" );
-
-		session.projects().values().forEach( project ->
-		{
-			System.out.println( "PROJECT " + project );
-			System.out.println( "DEPENDENCIES" );
-			session.graph().read().dependencies( project.getGav() ).forEach(System.out::println);
-
-			/**
-			 * Checks that transitive dependencies cannot be resolved
-			 */
-			TransitivityResolver resolver = new TransitivityResolver();
-			resolver.getTransitiveDependencyTree( session, project, true, true, null, new PomFileLoader()
-			{
-				@Override
-				public File loadPomFileForGav( Gav gav, List<Repository> additionalRepos, Log log )
-				{
-					assertTrue( shouldBeMissing.contains( gav.toString() ) );
-					shouldBeMissing.remove( gav.toString() );
-
-					return null;
-				}
-			}, System.out::println );
-		} );
-
-		assertTrue( shouldBeMissing.isEmpty() );
-	}
-
-	@Test
-	public void test06()
-	{
-		Session session = new Session();
-
-		PomAnalysis.runFullRecursiveAnalysis( "testSets/set06", session, null, null, true, System.out::println );
-
-		assertEquals( 4, session.projects().size() );
-
-		session.projects().values().forEach( project ->
-		{
-			System.out.println( "PROJECT " + project );
-			System.out.println( "DEPENDENCIES" );
-			session.graph().read().dependencies( project.getGav() ).forEach(System.out::println);
-
-			/**
-			 * Checks that transitive dependencies can be resolved
-			 */
-			TransitivityResolver resolver = new TransitivityResolver();
-			resolver.getTransitiveDependencyTree( session, project, true, true, null, new PomFileLoader()
-			{
-				@Override
-				public File loadPomFileForGav( Gav gav, List<Repository> additionalRepos, Log log )
-				{
-					fail( "missing gav " + gav + " but should not!" );
-
-					return null;
-				}
-			}, System.out::println );
-		} );
-	}
-
-	@Test
-	public void test07()
-	{
-		Session session = new Session();
-
-		PomAnalysis.runFullRecursiveAnalysis( "testSets/set07", session, null, null, true, System.out::println );
-
-		assertEquals( 2, session.projects().size() );
-
-		Project project = session.projects().forGav( Gav.parse( "fr.lteconsulting:a:1.0-SNAPSHOT" ) );
-		assertNotNull( project );
-
-		Map<GroupArtifact, String> pluginManagement = project.getHierarchicalPluginDependencyManagement( null, null, session.projects(), System.out::println );
-
-		assertEquals( 2, pluginManagement.size() );
-		assertEquals( "1.0-SNAPSHOT", pluginManagement.get( new GroupArtifact( "fr.lteconsulting", "plugin-a" ) ) );
-		assertEquals( "4", pluginManagement.get( new GroupArtifact( "fr.lteconsulting", "plugin-b" ) ) );
-
-		Set<Gav> plugins = project.getLocalPluginDependencies( null, session.projects(), System.out::println );
-
-		assertEquals( 3, plugins.size() );
-		assertTrue( plugins.contains( new Gav( "fr.lteconsulting", "plugin-a", "1.0-SNAPSHOT" ) ) );
-		assertTrue( plugins.contains( new Gav( "fr.lteconsulting", "plugin-b", "4" ) ) );
-		assertTrue( plugins.contains( new Gav( "fr.lteconsulting", "plugin-c", "5" ) ) );
-	}
-
-	@Test
-	public void test08()
-	{
-		Session session = new Session();
-
-		PomAnalysis.runFullRecursiveAnalysis( "testSets/set08", session, null, null, true, System.out::println );
-
-		assertEquals( 1, session.projects().size() );
-
-		Project project = session.projects().forGav( Gav.parse( "fr.lteconsulting:a:1.0-SNAPSHOT" ) );
-		assertNotNull( project );
+		//act
+		runFullRecursiveAnalysis(session, "testSets/set01");
+		//assert
+		assertProjects(session, 1);
+		assertDependenciesAndBuildDependencies(session, "fr.lteconsulting:pom-explorer:1.1-SNAPSHOT", 14, 2);
+		assertNullGavs(session, 1);
 	}
 
 	@Test
 	public void test02()
 	{
+		//arrange
 		Session session = new Session();
-
-		try
-		{
-			String directory;
-			// directory = "c:\\Documents\\Repos\\formation-programmation-java\\projets\\javaee\\cartes-webapp";
-			directory = "c:\\Documents\\Repos";
-			PomAnalysis.runFullRecursiveAnalysis( directory, session, new DefaultPomFileLoader( session, true ), null, true, System.out::println );
-		}
-		catch( Exception e )
-		{
-			e.printStackTrace();
-		}
-
-		System.out.println( "DEPENDENCIES" );
-		session.graph().read().dependencies( Gav.parse( "fr.lteconsulting:pom-explorer:1.1-SNAPSHOT" ) ).forEach(System.out::println);
-
-		System.out.println( "BUILD DEPENDENCIES" );
-		session.graph().read().buildDependencies( Gav.parse( "fr.lteconsulting:pom-explorer:1.1-SNAPSHOT" ) ).forEach(System.out::println);
-
-		System.out.println( "NULL VERSION GAVS" );
-		PomGraphReadTransaction tx = session.graph().read();
-		tx.gavs().stream()
-				.sorted(Comparator.comparing(Gav::toString))
-				.filter( gav -> gav.getVersion() == null )
-				.forEach( gav ->
-				{
-					System.out.println( gav );
-					tx.dependenciesRec( gav ).stream().forEach( r -> System.out.println( " - " + r ) );
-				} );
+		//act
+		runFullRecursiveAnalysis(session, "testSets/set02");
+		//assert
+		assertProjects(session, 5);
+		assertDependencies(session, PROJECT_A, 1);
+		assertDependencies(session, PROJECT_B, 1);
+		assertDependencies(session, PROJECT_C, 2);
+		assertDependencies(session, PROJECT_D, 0);
+		assertDependencies(session, PROJECT_E, 0);
+		assertNullGavs(session, 0);
 	}
 
 	@Test
 	public void test03()
 	{
+		//arrange
+		Session session = new Session();
+		//act
+		runFullRecursiveAnalysis(session, "testSets/set03");
+		//assert
+		assertProjects(session, 4);
+		assertDependencies(session, PROJECT_A, 2);
+		assertDependencies(session, PROJECT_B, 0);
+		assertDependencies(session, PROJECT_C, 1);
+		assertDependencies(session, PROJECT_D, 0);
+		assertNullGavs(session, 0);
+	}
+
+	@Test
+	public void test04()
+	{
+		//arrange
+		Session session = new Session();
+		//act
+		runFullRecursiveAnalysis(session, "testSets/set04");
+		//assert
+		assertProjects(session, 4);
+		assertDependencies(session, PROJECT_A, 2);
+		assertDependencies(session, PROJECT_B, 0);
+		assertDependencies(session, PROJECT_C, 1);
+		assertDependencies(session, PROJECT_D, 0);
+		assertNullGavs(session, 0);
+	}
+
+	@Test
+	public void test05()
+	{
+		//arrange
+		Session session = new Session();
+		//act
+		runFullRecursiveAnalysis(session, "testSets/set05");
+		//assert
+		assertProjects(session, 2);
+		assertDependencies(session, PROJECT_A, 1);
+		assertDependencies(session, PROJECT_B, 1);
+
+		List<String> shouldBeMissing = new ArrayList<>();
+		shouldBeMissing.add(PROJECT_D);
+		shouldBeMissing.add(PROJECT_C);
+		shouldBeMissing.add(PROJECT_C);
+
+		session.projects().values().forEach(project ->
+		{
+			System.out.println("PROJECT " + project);
+			System.out.println("DEPENDENCIES");
+			session.graph().read().dependencies(project.getGav()).forEach(System.out::println);
+
+			/**
+			 * Checks that transitive dependencies cannot be resolved
+			 */
+			TransitivityResolver resolver = new TransitivityResolver();
+			resolver.getTransitiveDependencyTree(session, project, true, true, null, new PomFileLoader()
+			{
+				@Override
+				public File loadPomFileForGav(Gav gav, List<Repository> additionalRepos, Log log)
+				{
+					assertTrue(shouldBeMissing.contains(gav.toString()));
+					shouldBeMissing.remove(gav.toString());
+
+					return null;
+				}
+			}, System.out::println);
+		});
+
+		assertTrue(shouldBeMissing.isEmpty());
+	}
+
+	@Test
+	public void test06()
+	{
+		//arrange
+		Session session = new Session();
+		//act
+		runFullRecursiveAnalysis(session, "testSets/set06");
+		//assert
+		assertProjects(session, 4);
+		assertDependencies(session, PROJECT_A, 1);
+		assertParentDependency(session, PROJECT_A, PROJECT_B);
+		assertDependencies(session, PROJECT_B, 1);
+		assertDependencies(session, PROJECT_C, 0);
+		assertDependencies(session, PROJECT_D, 0);
+
+		session.projects().values().forEach(project ->
+		{
+			System.out.println("PROJECT " + project);
+			System.out.println("DEPENDENCIES");
+			session.graph().read().dependencies(project.getGav()).forEach(System.out::println);
+
+			/**
+			 * Checks that transitive dependencies can be resolved
+			 */
+			TransitivityResolver resolver = new TransitivityResolver();
+			resolver.getTransitiveDependencyTree(session, project, true, true, null, new PomFileLoader()
+			{
+				@Override
+				public File loadPomFileForGav(Gav gav, List<Repository> additionalRepos, Log log)
+				{
+					fail("missing gav " + gav + " but should not!");
+
+					return null;
+				}
+			}, System.out::println);
+		});
+	}
+
+	@Test
+	public void test07()
+	{
+		//arrange
+		Session session = new Session();
+		//act
+		runFullRecursiveAnalysis(session, "testSets/set07");
+		//assert
+		assertProjects(session, 2);
+		assertDependencies(session, PROJECT_A, 1);
+		assertParentDependency(session, PROJECT_A, PROJECT_B);
+		assertDependencies(session, PROJECT_B, 0);
+
+		Project project = session.projects().forGav(Gav.parse("fr.lteconsulting:a:1.0-SNAPSHOT"));
+		assertNotNull(project);
+
+		Map<GroupArtifact, String> pluginManagement = project.getHierarchicalPluginDependencyManagement(null, null, session.projects(), System.out::println);
+
+		assertEquals(2, pluginManagement.size());
+		assertEquals("1.0-SNAPSHOT", pluginManagement.get(new GroupArtifact("fr.lteconsulting", "plugin-a")));
+		assertEquals("4", pluginManagement.get(new GroupArtifact("fr.lteconsulting", "plugin-b")));
+
+		Set<Gav> plugins = project.getLocalPluginDependencies(null, session.projects(), System.out::println);
+
+		assertEquals(3, plugins.size());
+		assertTrue(plugins.contains(new Gav("fr.lteconsulting", "plugin-a", "1.0-SNAPSHOT")));
+		assertTrue(plugins.contains(new Gav("fr.lteconsulting", "plugin-b", "4")));
+		assertTrue(plugins.contains(new Gav("fr.lteconsulting", "plugin-c", "5")));
+	}
+
+	@Test
+	public void test08()
+	{
+		//arrange
+		Session session = new Session();
+		//act
+		runFullRecursiveAnalysis(session, "testSets/set07");
+		//assert
+		assertProjects(session, 2);
+		assertDependencies(session, PROJECT_A, 1);
+
+		Project project = session.projects().forGav(Gav.parse("fr.lteconsulting:a:1.0-SNAPSHOT"));
+		assertNotNull(project);
+	}
+
+	@Test
+	public void localTest1()
+	{
+		Session session = new Session();
+
+		try {
+			String directory;
+			// directory = "c:\\Documents\\Repos\\formation-programmation-java\\projets\\javaee\\cartes-webapp";
+			directory = "c:\\Documents\\Repos";
+			PomAnalysis.runFullRecursiveAnalysis(directory, session, new DefaultPomFileLoader(session, true), null, true, System.out::println);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+		System.out.println("DEPENDENCIES");
+		session.graph().read().dependencies(Gav.parse("fr.lteconsulting:pom-explorer:1.1-SNAPSHOT")).forEach(System.out::println);
+
+		System.out.println("BUILD DEPENDENCIES");
+		session.graph().read().buildDependencies(Gav.parse("fr.lteconsulting:pom-explorer:1.1-SNAPSHOT")).forEach(System.out::println);
+
+		System.out.println("NULL VERSION GAVS");
+		PomGraphReadTransaction tx = session.graph().read();
+		tx.gavs().stream()
+				.sorted(Comparator.comparing(Gav::toString))
+				.filter(gav -> gav.getVersion() == null)
+				.forEach(gav ->
+				{
+					System.out.println(gav);
+					tx.dependenciesRec(gav).stream().forEach(r -> System.out.println(" - " + r));
+				});
+	}
+
+	@Test
+	public void localTest2()
+	{
 		Session session = new Session();
 
 		Log log = System.out::println;
 
-		DefaultPomFileLoader pomLoader = new DefaultPomFileLoader( session, true );
+		DefaultPomFileLoader pomLoader = new DefaultPomFileLoader(session, true);
 
-		PomAnalysis analyzis = new PomAnalysis( session, pomLoader, null, false, log );
+		PomAnalysis analyzis = new PomAnalysis(session, pomLoader, null, false, log);
 
-		analyzis.addDirectory( "c:\\Documents\\Repos" );
-		analyzis.addDirectory( "C:\\Users\\Arnaud\\.m2\\repository" );
+		analyzis.addDirectory("c:\\Documents\\Repos");
+		analyzis.addDirectory("C:\\Users\\Arnaud\\.m2\\repository");
 
 		analyzis.loadProjects();
 
@@ -222,10 +270,73 @@ public class AnalyzerTest
 
 		analyzis.addCompletedProjectsToGraph();
 
-		System.out.println( "GAVS" );
+		System.out.println("GAVS");
 		PomGraphReadTransaction tx = session.graph().read();
 		tx.gavs().stream()
 				.sorted(Comparator.comparing(Gav::toString))
 				.forEach(System.out::println);
+	}
+
+	private void runFullRecursiveAnalysis(Session session, String testSet)
+	{
+		PomAnalysis.runFullRecursiveAnalysis(testSet, session, null, null, true, System.out::println);
+	}
+
+	private void assertProjects(Session session, int numberOfProjects)
+	{
+		ProjectRepository projects = session.projects();
+		projects.values().forEach(project -> System.out.println("PROJECT " + project));
+		assertEquals("number of projects", numberOfProjects, projects.size());
+	}
+
+	private void assertDependenciesAndBuildDependencies(Session session, String gavString, int numberOfDependencies, int numberOfBuildDependencies)
+	{
+		assertDependencies(session, gavString, numberOfDependencies, "DEPENDENCIES RESULT FOR " + gavString + "\ndependencies:");
+		assertBuildDependencies(session, gavString, numberOfBuildDependencies);
+	}
+
+	private void assertDependenciesWithUnresolved(Session session, String gavString, int numberOfDependencies, String... unresolvedGavs)
+	{
+		assertDependencies(session, gavString, numberOfDependencies, "DEPENDENCIES RESULT FOR " + gavString + "\ndependencies:");
+		System.out.println("missing dependencies:");
+		//TODO find out how to retrieve unresolved dependencies from graph/session
+	}
+
+	private void assertDependencies(Session session, String gavString, int numberOfDependencies)
+	{
+		assertDependencies(session, gavString, numberOfDependencies, "DEPENDENCIES OF " + gavString);
+	}
+
+	private void assertDependencies(Session session, String gavString, int numberOfDependencies, String message)
+	{
+		System.out.println(message);
+		Set<DependencyRelation> dependencies = session.graph().read().dependencies(Gav.parse(gavString));
+		dependencies.forEach(System.out::println);
+		assertEquals("dependencies of " + gavString, numberOfDependencies, dependencies.size());
+	}
+
+	private void assertBuildDependencies(Session session, String gavString, int numberOfDependencies)
+	{
+		System.out.println("build dependencies:");
+		Set<BuildDependencyRelation> buildDependencies = session.graph().read().buildDependencies(Gav.parse(gavString));
+		buildDependencies.forEach(System.out::println);
+		assertEquals("build dependencies of " + gavString, numberOfDependencies, buildDependencies.size());
+	}
+
+	private void assertParentDependency(Session session, String gavString, String parentGav)
+	{
+		Gav parent = session.graph().read().parent(Gav.parse(gavString));
+		assertEquals("parent dependency of " + gavString, parent, Gav.parse(parentGav));
+	}
+
+	private void assertNullGavs(Session session, int numberOfNullGavs)
+	{
+		System.out.println("NULL VERSION GAVS");
+		List<Gav> nullGavs = session.graph().read().gavs().stream()
+				.filter(gav -> gav.getVersion() == null)
+				.sorted(Comparator.comparing(Gav::toString))
+				.collect(Collectors.toList());
+		nullGavs.forEach(System.out::println);
+		assertEquals("null gavs", numberOfNullGavs, nullGavs.size());
 	}
 }
