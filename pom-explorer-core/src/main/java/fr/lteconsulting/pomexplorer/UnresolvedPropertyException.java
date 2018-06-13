db4246ce7f149fec99cfdab63af3f42ee764cc76
Robert Stoll <robert.stoll@loewenfels.ch>
2018-06-13 15:11:02 +0200

    add getPropertyName to UnresolvedPropertyException

--- pom-explorer-core/src/main/java/fr/lteconsulting/pomexplorer/UnresolvedPropertyException.java
+++ pom-explorer-core/src/main/java/fr/lteconsulting/pomexplorer/UnresolvedPropertyException.java
@@ -3,16 +3,22 @@ package fr.lteconsulting.pomexplorer;
 
 public class UnresolvedPropertyException extends RuntimeException
 {
+	private final String propertyName;
 	private final Project project;
 
 	UnresolvedPropertyException( String propertyName, Project project )
 	{
 		super( "Could not resolve property " + propertyName + " of project " + project.getGav());
+		this.propertyName = propertyName;
 		this.project = project;
 	}
 
 	public Project getProject()
 	{
 		return project;
+	}
+	public String getPropertyName()
+	{
+		return propertyName;
 	}
 }
